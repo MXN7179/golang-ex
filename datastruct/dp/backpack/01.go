bff5d9e745c3830f9e8a1a4e7a334fa09a63f9b7
maxinning <maxinning@qianxin.com>
2022-07-15 18:06:21 +0800

    记忆化搜索

---
+++ datastruct/dp/backpack/01.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// 记忆化搜索
+// n个物品 (0-i)  (w,v)
+//0: (2, 3)
+//1: (1, 2)
+//2: (3, 4)
+//3: (2, 2)
+
+const MAX = 100
+
+var (
+	W  uint = 5
+	n  uint = 4
+	w       = [MAX]uint{2, 1, 3, 2}
+	v       = [MAX]uint{3, 2, 4, 2}
+	dp [MAX][MAX]uint
+)
+
+func rec(i, j uint) uint {
+	if i == n {
+		return 0
+	}
+
+	if j < w[i] {
+		return rec(i+1, j)
+	} else {
+		return uint(math.Max(float64(rec(i+1, j)), float64(rec(i+1, j-w[i])+v[i])))
+	}
+
+}
+
+// 记忆化搜索
+// n个物品 (0-i)  (w,v)
+func main() {
+	for j := uint(0); j <= W; j++ { // 初始化 dp[n][j]
+		dp[n][j] = 0
+	}
+
+	res := rec(0, W)
+	fmt.Println(res)
+}
