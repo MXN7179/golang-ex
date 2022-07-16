f7a1693e4613bee823d047d3e23b51488c2b75e5
maxinning <maxinning@qianxin.com>
2022-07-17 00:57:48 +0800

    动态规划

--- datastruct/dp/backpack/01.go
+++ datastruct/dp/backpack/01.go
@@ -5,7 +5,6 @@ import (
 	"math"
 )
 
-// 记忆化搜索
 // n个物品 (0-i)  (w,v)
 //0: (2, 3)
 //1: (1, 2)
@@ -15,32 +14,52 @@ import (
 const MAX = 100
 
 var (
-	W  uint = 5
-	n  uint = 4
-	w       = [MAX]uint{2, 1, 3, 2}
-	v       = [MAX]uint{3, 2, 4, 2}
-	dp [MAX][MAX]uint
+	W  int = 5
+	n  int = 4
+	w      = [MAX]int{2, 1, 3, 2}
+	v      = [MAX]int{3, 2, 4, 2}
+	dp [MAX][MAX]int
 )
 
-func rec(i, j uint) uint {
-	var res uint
+// 记忆化搜索
+func rec(i, j int) int {
+	var res int
 	if i == n {
 		res = 0
 	} else if j < w[i] {
 		res = rec(i+1, j)
 	} else {
-		res = uint(math.Max(float64(rec(i+1, j)), float64(rec(i+1, j-w[i])+v[i])))
+		res = int(math.Max(float64(rec(i+1, j)), float64(rec(i+1, j-w[i])+v[i])))
 	}
 	return res
 }
-
-// 记忆化搜索
-// n个物品 (0-i)  (w,v)
-func main() {
-	for j := uint(0); j <= W; j++ { // 初始化 dp[n][j]
+func initDp() {
+	for j := int(0); j <= W; j++ { // 初始化 dp[n][j]
 		dp[n][j] = 0
 	}
+}
 
-	res := rec(0, W)
-	fmt.Println(res)
+// 动态规划
+func solve() {
+	initDp()
+
+	for i := n - 1; i >= 0; i-- {
+		for j := 0; j <= W; j++ {
+			if j < w[i] {
+				dp[i][j] = dp[i+1][j]
+			} else {
+				dp[i][j] = int(math.Max(float64(dp[i+1][j]), float64(dp[i+1][j-w[i]]+v[i])))
+			}
+		}
+	}
+
+	fmt.Println(dp[0][W])
+}
+
+func main() {
+	//initdp()
+	//res := rec(0, W)
+	//fmt.Println(res)
+
+	solve()
 }
