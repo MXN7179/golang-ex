ee6a168d6ec7ed16f5ec99147f44b27099ee4919
maxinning <13371000700@163.com>
2022-07-17 14:43:05 +0800

    正向推导

--- datastruct/dp/backpack/01.go
+++ datastruct/dp/backpack/01.go
@@ -11,13 +11,15 @@ import (
 //2: (3, 4)
 //3: (2, 2)
 
-const MAX = 100
+const MAX = 10
 
 var (
-	W  int = 5
-	n  int = 4
-	w      = [MAX]int{2, 1, 3, 2}
-	v      = [MAX]int{3, 2, 4, 2}
+	W  = 5
+	n  = 4
+	w  = [MAX]int{2, 1, 3, 2}
+	v  = [MAX]int{3, 2, 4, 2}
+	w2 = [MAX]int{0, 2, 1, 3, 2}
+	v2 = [MAX]int{0, 3, 2, 4, 2}
 	dp [MAX][MAX]int
 )
 
@@ -33,15 +35,15 @@ func rec(i, j int) int {
 	}
 	return res
 }
-func initDp() {
+func initDp(row int) {
 	for j := int(0); j <= W; j++ { // 初始化 dp[n][j]
-		dp[n][j] = 0
+		dp[row][j] = 0
 	}
 }
 
 // 动态规划
 func solve() {
-	initDp()
+	initDp(n)
 
 	for i := n - 1; i >= 0; i-- {
 		for j := 0; j <= W; j++ {
@@ -56,10 +58,26 @@ func solve() {
 	fmt.Println(dp[0][W])
 }
 
+// dp 正向推导
+func solve2() {
+	initDp(0)
+
+	for i := 1; i <= n; i++ {
+		for j := 0; j <= W; j++ {
+			if j < w2[i] {
+				dp[i][j] = dp[i-1][j]
+			} else {
+				dp[i][j] = int(math.Max(float64(dp[i-1][j]), float64(dp[i-1][j-w2[i]]+v2[i])))
+			}
+		}
+	}
+
+	fmt.Println(dp)
+}
 func main() {
 	//initdp()
 	//res := rec(0, W)
 	//fmt.Println(res)
 
-	solve()
+	solve2()
 }
