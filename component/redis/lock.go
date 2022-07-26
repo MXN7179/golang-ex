4c495137c4960b92c837a773d9211c0a1ed5fe09
maxinning <13371000700@163.com>
2022-07-26 18:52:41 +0800

    看门狗机制

--- component/redis/lock.go
+++ component/redis/lock.go
@@ -16,40 +16,40 @@ const (
 		return 0
 	end
 `
+	renewalStr = `
+	  if redis.call("get", KEYS[1]) == ARGV[1] then
+		return redis.call("expire", ARGV[2])
+	  else
+		return 0
+	  end
+`
 )
 
 var(
- 	//ctx = context.Background()
-	//client *redis.Client
-
 	value int
 	key string
+
+	renewalCh = make(chan struct{})
 )
 func init(){
-	//initRedis()
-	initA()
-}
-func initRedis() {
-	client = redis.NewClient(&redis.Options{
-		Addr: "127.0.0.1:6379",
-		Password: "",
-		DB: 0,
-	})
-}
-func initA() {
 	rand.Seed(time.Now().Unix())
 	value = rand.Intn(1000)
 	key = "key1"
 }
+func WatchDog(requestId string){
+	ticker := time.NewTicker(3 * time.Second)
+	renewalScript := redis.NewScript(renewalStr)
 
-func main() {
-	Lock()
-	defer UnLock()
-	// do ...
-	time.Sleep(time.Second * 3)
-
+	for {
+		select {
+		case <- ticker.C:
+			fmt.Println("续期5s")
+			renewalScript.Run(ctx, client, []string{key}, requestId, 5)  // 续期5秒
+		case <- renewalCh:
+			return
+		}
+	}
 }
-
 func Lock(){
 	succ, err := client.SetNX(ctx, key, strconv.Itoa(value), time.Second * 5).Result()
 	if err != nil{
@@ -58,6 +58,7 @@ func Lock(){
 		fmt.Printf("key has exists\n")
 	}else{
 		fmt.Printf("key set success\n")
+		go WatchDog(strconv.Itoa(value))	 // 开启看门狗
 	}
 }
 
@@ -65,4 +66,21 @@ func UnLock(){
 	unlockScript := redis.NewScript(unlockStr)
 	res, err := unlockScript.Run(ctx, client, []string{key}, strconv.Itoa(value)).Result()
 	fmt.Printf("res: %v err: %v\n", res, err)
+	if err != nil{
+		fmt.Println("unlock fail")
+	}else if res == 0 {
+		fmt.Println("lock not exists")
+	}else{
+		fmt.Println("renewal <- ")
+		renewalCh <- struct{}{}
+	}
+}
+
+func main() {
+	Lock()
+	defer UnLock()
+	// do ...
+	time.Sleep(time.Second * 8)
+
+
 }
